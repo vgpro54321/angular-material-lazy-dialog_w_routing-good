1545bb6ead9d8aa93496270fd0c7611c95a2d8db
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-01-23 07:45:56 -0500

    1 file modified

--- src/app/dialog/components/screen3/screen3.component.ts
+++ src/app/dialog/components/screen3/screen3.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: 'screen3.component.html',
 })
 export class Screen3Component {
-  constructor(private router: Router) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   goToScreen2(): void {
-    this.router.navigate(['/dialog/screen2'], {
-      skipLocationChange: true,
+    this.router.navigate(['screen2'], {
+      //skipLocationChange: true,
+      relativeTo: this.route.parent,
     });
   }
 
